a13e52837168835b02f1e3b06dcf1c49d0ef8452
DARSHAN RAVAL <110303703+FaceHide7@users.noreply.github.com>
2022-07-31 05:07:04 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello PHP World</p>'; 
+      echo 'My name Is Lakahan';
+    ?> 
+    
+    <script src="https://replit.com/public/js/replit-badge.js" theme="blue" defer></script> 
+  </body>
+</html>
